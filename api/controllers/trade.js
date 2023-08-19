23baaaac21998e6f5d658449fbadac1640531c14
Pratik <singhpratik015@gmail.com>
2023-08-19 17:53:25 +0530

    quote updated

--- api/controllers/trade.js
+++ api/controllers/trade.js
@@ -27,11 +27,11 @@ const Quote =  async(req,res)=>{
     // const src= req.src || 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     // const dst = req.dst || 0x111111111117dc0aa78b770fa6a738034120c302;
     // const amount = req.amount * 1e18 || 10000000000000000;
-    // const src= "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
-    // const dst = "0x111111111117dc0aa78b770fa6a738034120c302";
-    // const amount = "10000000000000000";
-    // const apiUrl = `https://api.1inch.dev/swap/v5.2/1/quote?src=${src}&dst=${dst}&amount=${amount}`
-    const apiUrl = "https://api.1inch.dev/swap/v5.2/1/quote?src=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&dst=0x111111111117dc0aa78b770fa6a738034120c302&amount=10000000000000000";
+    const src = req.body.src;
+    const dst = req.body.dst;
+    const amount = req.body.amount;
+    const apiUrl = `https://api.1inch.dev/swap/v5.2/1/quote?src=${src}&dst=${dst}&amount=${amount}`
+    // const apiUrl = "https://api.1inch.dev/swap/v5.2/1/quote?src=0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE&dst=0x111111111117dc0aa78b770fa6a738034120c302&amount=10000000000000000";
     const authToken = 'YHIkhFVEk5PyshRZy5i9zboVDHk8A0Bm'; 
 
     const headers = {
--- api/routes/trade.js
+++ api/routes/trade.js
@@ -6,7 +6,7 @@ const TradeControllers = require("../controllers/trade.js");
 //     res.send("hello");
 // })
 router.get("/tokens",TradeControllers.Tokens);
-router.get("/quote",TradeControllers.Quote);
+router.post("/quote",TradeControllers.Quote);
 
 module.exports = router;
 // export default router;
