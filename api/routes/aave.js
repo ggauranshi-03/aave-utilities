bff3499d2a961059014ead4d0acfa7d507bf59dd
Pratik <singhpratik015@gmail.com>
2023-08-19 13:13:54 +0530

    deploying

--- api/routes/aave.js
+++ api/routes/aave.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const AaveControllers = require("../controllers/aave.js");
 
+router.get("/",async(req,res)=>{
+    res.send("hello");
+})
 router.get("/aave",AaveControllers.Data);
 
 module.exports = router;
